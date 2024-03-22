a46db2faa6b7a40cc82047313eb345fc7017fba8
test <test@test.com>
2024-03-22 18:50:05 +0900

    3/21 학원

--- src/main/java/com/quiz/lesson01/Lesson01Quiz03Controller.java
+++ src/main/java/com/quiz/lesson01/Lesson01Quiz03Controller.java
@@ -8,6 +8,10 @@ public class Lesson01Quiz03Controller {
 
 	@RequestMapping("/lesson01/quiz03")
 	public String quiz03() {
+		// ResponseBody가 아닌 상태로 return String을 하면
+		// ViewResolver에 의해 리턴된 String jsp 경로를 찾고
+		// jsp => html 화면이 구성된다.
+		
 		return "lesson01/quiz03";
 	}
 }
