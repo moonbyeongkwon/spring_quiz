da5b797efe4f619a3dbe34db064a85c71f5886fe
test <test@test.com>
2024-04-11 21:41:02 +0900

    4/11 학원

---
+++ src/main/java/com/quiz/lesson07/bo/CompanyBO.java
@@ -0,0 +1,45 @@
+package com.quiz.lesson07.bo;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.quiz.lesson07.entity.CompanyEntity;
+import com.quiz.lesson07.repository.CompanyRepository;
+
+@Service
+public class CompanyBO {
+	
+	@Autowired
+	private CompanyRepository companyRepository;
+	
+	public CompanyEntity addCompany(String name, String business, String scale, int headcount) {
+		
+		return companyRepository.save(CompanyEntity.builder()
+				.name(name)
+				.business(business)
+				.scale(scale)
+				.headcount(headcount)
+				.build());
+	}
+	
+	// update 
+	public CompanyEntity updateCompanyById(int id, String scale, int headcount) {
+		CompanyEntity company = companyRepository.findById(id).orElse(null);
+		if (company != null) {
+			company = company.toBuilder()
+				.scale(scale)
+				.headcount(headcount)
+				.build();
+			company = companyRepository.save(company);
+		}
+		
+		return company;
+	}
+	public void deleteCompanyById(int id) {
+		Optional<CompanyEntity> companyOptional = companyRepository.findById(id);
+		companyOptional.ifPresent(s -> companyRepository.delete(s));
+	}
+	
+}
